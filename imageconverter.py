b66c147ec0bd354b35886ec63f40381cc2e42bab
austinsarwar <austin_sarwar@yahoo.com>
2023-01-16 18:46:11 -0500

    Make functional image conversion application

--- imageconverter.py
+++ imageconverter.py
@@ -1,25 +1,53 @@
-from PIL import Image  # Python Image Library - Image Processing
-import glob
+from PIL import Image
 import tkinter as tk
-from tkinter import Button, filedialog
+from tkinter import Button, Label, filedialog
 import os
+
+# Create the main window
 root = tk.Tk()
 root.geometry("300x200")
 root.title("Image converter")
-select_button = Button(root,text="Select File")
-select_button.pack()
+
+# Initialize the filepath variable
+filepath = ""
+
+# Define the select_file function
+def select_file():
+    global filepath
+    filepath = filedialog.askopenfilename()
+
+# Define the file_convert function
+def file_convert(filepath):
+    try:
+        # Open the image and convert it to RGB
+        im = Image.open(filepath)
+        im = im.convert('RGB')
+        
+        # Create the new filepath and save the image
+        new_filepath = os.path.splitext(filepath)[0] + '.jpg'
+        im.save(new_filepath, format='JPEG', quality=95)
+        
+        # Display the success message
+        success = Label(root, text="Conversion success")
+        success.grid(row=2,column=0,sticky=tk.NSEW)
+    except FileNotFoundError:
+        # Display the file not found message
+        not_found = Label(root, text="File not found")
+        not_found.grid(row=1,column=1,sticky=tk.NSEW)
+    except OSError:
+        # Display the invalid image message
+        error = Label(root, text="The selected file is not a valid image.")
+        error.grid(row=2,column=0,sticky=tk.NSEW)
+
+# Create the select file button
+select_button = Button(root, text="Select File", command=lambda: select_file())
+select_button.grid(row=0, column=0,sticky=tk.NSEW)
 select_button.config(font=("Arial",20))
-convert_button = Button(root, text="Convert File")
+
+# Create the convert file button
+convert_button = Button(root, text="Convert File", command=lambda: file_convert(filepath))
+convert_button.grid(row=1, column=0,sticky=tk.NSEW)
 convert_button.config(font=("Arial",20))
-convert_button.pack()
 
-
-def file_convert(file):
-     for file in glob.glob("*.png"):
-          im = Image.open(file)
-          rgb_im = im.convert('RGB')
-          rgb_im.save(file.replace("png", "jpg"), quality=95)
-
-
-
+# Start the main event loop
 root.mainloop()
