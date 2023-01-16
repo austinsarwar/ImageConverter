0e4e116e3749da5a1fa106945dca0dafa39110ab
austinsarwar <austin_sarwar@yahoo.com>
2023-01-16 17:38:22 -0500

    Set up main window

--- imageconverter.py
+++ imageconverter.py
@@ -1,6 +1,25 @@
 from PIL import Image  # Python Image Library - Image Processing
 import glob
-for file in glob.glob("*.png"):
-     im = Image.open(file)
-     rgb_im = im.convert('RGB')
-     rgb_im.save(file.replace("png", "jpg"), quality=95)
+import tkinter as tk
+from tkinter import Button, filedialog
+import os
+root = tk.Tk()
+root.geometry("300x200")
+root.title("Image converter")
+select_button = Button(root,text="Select File")
+select_button.pack()
+select_button.config(font=("Arial",20))
+convert_button = Button(root, text="Convert File")
+convert_button.config(font=("Arial",20))
+convert_button.pack()
+
+
+def file_convert(file):
+     for file in glob.glob("*.png"):
+          im = Image.open(file)
+          rgb_im = im.convert('RGB')
+          rgb_im.save(file.replace("png", "jpg"), quality=95)
+
+
+
+root.mainloop()
